f925f97bb00ed43207bfaf7934a3bbbce9c02b76
Mike Tai <aguiarmichael20@outlook.com>
2025-06-25 20:18:26 -0300

    ajuste da rota da pagina About pelo Navbar.jsx²

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -88,7 +88,7 @@ const Navbar = () => {
           <FaHeart className="icon" />
           <span>Favoritos</span>
         </NavLink>
-        <NavLink to="/sobre" className="nav-link">
+        <NavLink to="/about" className="nav-link">
           <FaInfoCircle className="icon" />
           <span>Sobre</span>
         </NavLink>
